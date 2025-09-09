8ffb4689bb08288bc93cc6848fc423e857d9d2d5
Aliza Held <aliza.held@strato.de>
2025-09-09 08:49:40 +0200

    IONOS(feat): add translations
    l10n folder containing en, de, es, fr, it, nl and sv translation files
    
    Signed-off-by: Aliza Held <aliza.held@strato.de>

---
+++ l10n/nl.js
@@ -0,0 +1,11 @@
+OC.L10N.register(
+    "mailtemplate",
+    {
+    "Best regards" : "Vriendelijke groeten",
+    "Customised email templates for Nextcloud" : "Aangepaste e-mailsjablonen voor Nextcloud",
+    "Further information" : "Meer informatie",
+    "Germany" : "Duitsland",
+    "List item icon" : "Lijstitem pictogram",
+    "Mail Template" : "Mail Sjabloon"
+},
+"nplurals=2; plural=(n==1) ? 0 : 1;");
