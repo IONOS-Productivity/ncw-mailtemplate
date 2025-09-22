a1703c07f5cc327635c923d7e308592ff8c1e2f1
Aliza Held <aliza.held@strato.de>
2025-09-22 11:41:01 +0200

    IONOS(refactor): rename app id
    Signed-off-by: Aliza Held <aliza.held@strato.de>

--- lib/AppInfo/Application.php
+++ lib/AppInfo/Application.php
@@ -10,7 +10,7 @@ use OCP\AppFramework\Bootstrap\IBootstrap;
 use OCP\AppFramework\Bootstrap\IRegistrationContext;
 
 class Application extends App implements IBootstrap {
-	public const APP_ID = 'mailtemplate';
+	public const APP_ID = 'ncw_mailtemplate';
 
 	/** @psalm-suppress PossiblyUnusedMethod */
 	public function __construct() {
